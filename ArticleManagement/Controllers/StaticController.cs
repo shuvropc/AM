b9435d8872824279e01ac6d9abc1909352de29e7
Amir Hamza Md Kayes <amir_akayes@yahoo.com>
2020-06-27 00:13:36 +0600

    AASDSD

---
+++ ArticleManagement/Controllers/StaticController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArticleManagement.Controllers
+{
+    public class StaticController : Controller
+    {
+        public IActionResult HomeStatic()
+        {
+            return View();
+        }
+        public IActionResult GuideLinesMenuScripts()
+        {
+            return View();
+        }
+        public IActionResult SubmitReview()
+        {
+            return View();
+        }
+        public IActionResult JSDReviewerInstruction()
+        {
+            return View();
+        }
+        public IActionResult GuideLinesForAcceptedArticles()
+        {
+            return View();
+        }
+        public IActionResult ReplicationFilesandArticleVerificationProcess()
+        {
+            return View();
+        }
+        public IActionResult SectionsoftheManuscript()
+        {
+            return View();
+        }
+        public IActionResult SubmissionProcessforFinalDraft()
+        {
+            return View();
+        }
+        public IActionResult EditorialBoard()
+        {
+            return View();
+        }
+    }
+}
