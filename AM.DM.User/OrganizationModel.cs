68ef64067f4166c74db39c954c572bf28d16ebad
Shuvro <37395674+shuvropc@users.noreply.github.com>
2021-03-23 02:21:27 +0600

    Org Revision 02

--- AM.DM.User/OrganizationModel.cs
+++ AM.DM.User/OrganizationModel.cs
@@ -6,7 +6,7 @@ namespace AM.DM.User
 {
     public class OrganizationModel
     {
-        //https://github.com/shuvropc/AM/edit/master/AM.DM.User/OrganizationModel.cs This is a link
+        //https://github.com/shuvropc/AM/edit/master/AM.DM.User/OrganizationModel.cs This is a link looks good
         public long Id { get; set; }
         public string OrganizationName { get; set; }
     }
