52972cc8eb2833f1064e9b9d00d958e0dbebcbfe
Shuvro <37395674+shuvropc@users.noreply.github.com>
2021-03-23 01:23:23 +0600

    Revision 01

--- AM.BLL.Common/Core/IEmailHandlerService.cs
+++ AM.BLL.Common/Core/IEmailHandlerService.cs
@@ -6,6 +6,7 @@ namespace AM.BLL.Common.Core
 {
     public interface IEmailHandlerService
     {
+        //https://github.com/shuvropc/AM/edit/master/AM.BLL.Common/Core/IEmailHandlerService.cs
         public void SendEmail(string pToEmail, string pSubject, string pBody);
 
     }
